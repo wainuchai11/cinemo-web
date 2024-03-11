820bb85ead85688a52c8aa867b6128db00f89ad7
Winuchai Phaichan <winuchai@azurecomputer.com>
2024-03-12 05:16:53 +0700

    Refactor Modal component import statement

--- src/app/components/Modal.jsx
+++ src/app/components/Modal.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 const Modal = ({ isOpen, onClose, children }) => {
     const handleOverlayClick = (e) => {
